3bd20cbec004818b74ec9ab863890110bdbee202
guruiaint <85266391+guruiaint@users.noreply.github.com>
2021-09-28 16:30:00 -0700

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,42 @@
+from django.urls.base import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView, 
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
